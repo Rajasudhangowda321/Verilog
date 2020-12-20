92d218b5c871c9555dd1d62953497586ecd2df9e
Rajasudhangowda321 <69045108+Rajasudhangowda321@users.noreply.github.com>
2020-12-20 22:44:27 +0530

    Create Notgate_bh.v

---
+++ Logicgates/Notgate_bh.v
@@ -0,0 +1,22 @@
+module not_bh(a,y);
+ input a;
+ output y;
+ reg y;
+ wire x;
+ assign x={a};
+ always@(x)
+ begin
+	if (x==1'b0)
+	y=1'b1;else y=1'b0;
+ end
+endmodule
+module testbench_notg;
+ reg a;wire y;
+ not_gate NG(a,y);
+ initial begin
+   $display($time,"a=%b,y=%b",a,y);
+   #10 a=0;
+   #10 a=1;
+   #10 $finish;
+ end
+endmodule
