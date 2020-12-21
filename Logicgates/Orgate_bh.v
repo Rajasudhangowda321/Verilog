4925f92a7296fb937c4274cfdab3aa20f93acb64
Rajasudhangowda321 <69045108+Rajasudhangowda321@users.noreply.github.com>
2020-12-21 18:39:22 +0530

    Update Orgate_bh.v

--- Logicgates/Orgate_bh.v
+++ Logicgates/Orgate_bh.v
@@ -1,4 +1,4 @@
-module or_gate_bh(a,b,y);
+module orgate_bh(a,b,y);
  input a,b;output y;
  reg y;wire[1:0]x;
  assign x={a,b};
@@ -8,9 +8,9 @@ module or_gate_bh(a,b,y);
 	y=1'b0;else y=1'b1;
  end
 endmodule
-module testbench_org_bh;
+module testbench_org;
  reg a,b;wire y;
- or_gate org(a,b,y);
+ orgate_bh org(a,b,y);
  initial begin
 	$monitor($time,"a=%b,b=%b,y=%b",a,b,y);
 	#10 a=0;b=0;
